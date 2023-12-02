3c811d013e98a6dc9ab422203e327133e4b581fa
Richard Nyström <ricn@rny.io>
2023-12-03 00:41:16 +0100

    Day 2 part 2

--- lib/two.ex
+++ lib/two.ex
@@ -8,6 +8,14 @@ defmodule Two do
     |> Enum.sum()
   end
 
+  def run2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_game(&1))
+    |> Enum.map(& &1.power)
+    |> Enum.sum()
+  end
+
   defp possible?(game, configuration) do
     Enum.all?(game.sets, fn set ->
       Enum.all?(set, fn {color, amount} ->
@@ -41,6 +49,30 @@ defmodule Two do
         end)
       end)
 
-    %{no: String.to_integer(no), sets: sets}
+    maxes =
+      sets
+      |> List.flatten()
+      |> Enum.reduce(%{max_red: 0, max_green: 0, max_blue: 0}, fn {color, amount}, acc ->
+        cond do
+          color == "red" && acc.max_red < amount ->
+            %{max_red: amount, max_green: acc.max_green, max_blue: acc.max_blue}
+
+          color == "green" && acc.max_green < amount ->
+            %{max_red: acc.max_red, max_green: amount, max_blue: acc.max_blue}
+
+          color == "blue" && acc.max_blue < amount ->
+            %{max_red: acc.max_red, max_green: acc.max_green, max_blue: amount}
+
+          true ->
+            acc
+        end
+      end)
+      |> dbg
+
+    %{
+      no: String.to_integer(no),
+      sets: sets,
+      power: maxes.max_red * maxes.max_green * maxes.max_blue
+    }
   end
 end
--- test/two_test.exs
+++ test/two_test.exs
@@ -23,4 +23,22 @@ defmodule TwoTest do
 
     assert Two.run(configuration, input) == 1853
   end
+
+  test "part 2 with example input" do
+    input = """
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    """
+
+    assert Two.run2(input) == 2286
+  end
+
+  test "part 2 with real input" do
+    {:ok, input} = File.read(@input_file)
+
+    assert Two.run2(input) == 72706
+  end
 end
