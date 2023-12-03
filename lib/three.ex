ddba362e745dd4d896459eb4218bbf27e2087c78
Richard Nyström <ricn@rny.io>
2023-12-03 20:46:53 +0100

    Day 3 part 2

--- lib/three.ex
+++ lib/three.ex
@@ -32,4 +32,33 @@ defmodule Three do
     |> Stream.map(&elem(&1, 2))
     |> Enum.sum()
   end
+
+  def run2(input) do
+    lines = input |> String.split("\n") |> Enum.with_index()
+
+    numbers =
+      lines
+      |> Enum.flat_map(fn {line, i} ->
+        Regex.scan(~r/\d+/, line, return: :index)
+        |> List.flatten()
+        |> Enum.map(fn {j, len} ->
+          {(i - 1)..(i + 1)//1, (j - 1)..(j + len)//1,
+           String.to_integer(String.slice(line, j, len))}
+        end)
+      end)
+
+    lines
+    |> Stream.flat_map(fn {line, i} ->
+      Regex.scan(~r/\*/, line, return: :index)
+      |> List.flatten()
+      |> Enum.map(fn {j, _} -> {i, j} end)
+    end)
+    |> Stream.map(fn {i, j} ->
+      case Enum.filter(numbers, fn {row_span, col_span, _n} -> i in row_span and j in col_span end) do
+        [a, b] -> elem(a, 2) * elem(b, 2)
+        _ -> 0
+      end
+    end)
+    |> Enum.sum()
+  end
 end
--- test/three_test.exs
+++ test/three_test.exs
@@ -22,7 +22,28 @@ defmodule ThreeTest do
 
   test "with real input" do
     {:ok, input} = File.read(@input_file)
-
     assert Three.run(input) == 551_094
+  end
+
+  test "part 2 with example input" do
+    input = """
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..
+    """
+
+    assert Three.run2(input) == 467_835
+  end
+
+  test "part 2 with real input" do
+    {:ok, input} = File.read(@input_file)
+    assert Three.run2(input) == 80_179_647
   end
 end
